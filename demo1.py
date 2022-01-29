1b91cbf7b4c41aef7604109a14619d2952175c14
chris-ruiz <ccibinjoseph200l@gmail.cpm>
2022-01-29 13:05:41 +0400

    one line

--- demo1.py
+++ demo1.py
@@ -1,2 +1,12 @@
-print("Hello pycharm")
-print("hi")
+import mysql.connector as c
+con=c.connect(host="localhost",user="root",password="mint",database="school")
+if con.is_connected():
+    print("Successfully Connected...")
+else:
+    print("Connectivty issues...")
+
+mycursor=con.cursor()
+mycursor.execute("desc account")
+for i in mycursor:
+    print(i)
+
